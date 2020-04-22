a797a7385b61235685bb762b8216901837943641
jackg235 <jackgoettle23@gmail.com>
2020-04-22 15:14:03 -0400

    integrated backend with react

---
+++ server/db-config.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+  host: "cis450project.c7pyba5j5lto.us-east-1.rds.amazonaws.com",
+  user: "apje_project",
+  password: "A1s2d3f4G54321",
+  database: "ApjeProj"
+};
---
+++ server/index.js
@@ -0,0 +1,15 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+var routes = require("./routes.js");
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// console.log that your server is up and running
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+// create a GET route
+app.get('/express_backend', (req, res) => {
+  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+});
