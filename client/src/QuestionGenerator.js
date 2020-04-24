6ab0dd39a743a8a6c1e1e16eecc35bdbf7a18605
alexwyso <alexwyso@seas.upenn.edu>
2020-04-24 16:16:30 -0400

    changes to question generator

--- client/src/QuestionGenerator.js
+++ client/src/QuestionGenerator.js
@@ -1,17 +1,18 @@
 
 class QuestionGenerator {
 
-	var stats = [];
-	var years = [];
-	var teams = [];
+	stats = [];
+	years = [];
+	teams = [];
 
 	constructor() {
-		this.stats = getStatOptions();
+		this.stats = getStatOptions(git );
 		this.years = getYearOptions();
 		this.teams = getTeamOptions();  
 	}
 
 	getStatOptions() {
+		// TODO get these from database call
 		return ["STAT1", "STAT2"]
 	}
 
@@ -29,35 +30,83 @@ class QuestionGenerator {
 		var teamIndex = Math.randomInt(0, this.teams.length());
 		var questionIndex = Math.randomInt(0, 6);
 
-		String question = "";
-		String query = "";
+		var stat = this.stats[statIndex];
+		var year = this.years[yearIndex];
+		var team = this.teams[teamIndex];
+
+		var question = "";
+		var query = "";
 
 		switch(questionIndex) {
 		  	case 0:
-		  		question = "Which player had the most " + statIndex + " in " + yearIndex + "?"
-		    	return []
+		  		question = "Which player had the most ${stat} in ${year}?"
+		  		query = "WITH Stat AS (" +
+							"SELECT name, ${stat}" +
+							"FROM Batting" +
+							"WHERE yearID == ${year}" +
+						"SELECT name, MAX(${stat})" +
+						"FROM Stat" +
+						"GROUP BY ${stat}"
+						"HAVING ROWNUM == 1"
+
+		    	return [question, query]
 		    	break;
 		  	case 1:
-		    	question = "Which player had the fewest " + statIndex + " in " + yearIndex + "?"
+		    	question = "Which player had the fewest ${stat} in ${year}?"
+		    	query = "WITH Stat AS (" +
+							"SELECT name, ${stat}" +
+							"FROM Batting" +
+							"WHERE yearID == ${year}" +
+						"SELECT name, MIN(${stat})" +
+						"FROM Stat" +
+						"GROUP BY ${stat}"
+						"HAVING ROWNUM == 1"
+
+		    	return [question, query]
 		   		break;
 	   		case 2:
-		    
+		    	question = "Which pitcher had the most ${stat} in ${year}?"
+				query = "WITH Stat AS (" +
+							"SELECT name, ${stat}" +
+							"FROM Pitching" +
+							"WHERE yearID == ${year}" +
+						"SELECT name, MAX(${stat})" +
+						"FROM Stat" +
+						"GROUP BY ${stat}"
+						"HAVING ROWNUM == 1"
+
+		    	return [question, query]
 		   		break;
 	   		case 3:
+	   			question = "Which pitcher had the fewest ${stat} in ${year}?"
+	   			query = "WITH Stat AS (" +
+							"SELECT name, ${stat}" +
+							"FROM Pitching" +
+							"WHERE yearID == ${year}" +
+						"SELECT name, MIN(${stat})" +
+						"FROM Stat" +
+						"GROUP BY ${stat}"
+						"HAVING ROWNUM == 1"
 
+	   			return [question, query]
 		   		break;
 	   		case 4:
 
+	   			return [question, query]
 		   		break;
 	   		case 5:
-
+	   			question = "Who is the lightest player to hit 50 home-runs in a season?";
+	   			query = "WITH Weights AS (" +
+							"SELECT playerID, nameFirst, nameLast, weight" +
+							"FROM People)" +
+						"SELECT nameFirst, nameLast, MIN(weight)" +
+						"FROM Weights w JOIN Batting b ON b.playerID == w.playerID" +
+						"GROUP BY weight" +
+						"HAVING HR > 50";
+	   			return [question, query]
 		   		break;
 		  	default:
 
 		}
-		
 	}
-
-
-
 }
