d8374686e2b89edb11b531ef3e6b450b41ca79c3
alexwyso <alexwyso@seas.upenn.edu>
2020-05-01 01:47:45 -0400

    connected fully to DB, still a dummy query. Top 10 results for a query returned, correct answer and 3 randoms selected. added score keeping.

--- client/src/App.js
+++ client/src/App.js
@@ -17,18 +17,26 @@ class App extends Component {
       choice1: "",
       choice2: "",
       choice3: "",
-      result: ""
+      result: "Good Luck!",
+      countCorrect: 0,
+      countQuestions: 0
     };
 
     this.getRandomQuestion = this.getRandomQuestion.bind(this);
-    this.click0 = this.click0.bind(this);
-    this.click1 = this.click1.bind(this);
-    this.click2 = this.click2.bind(this);
-    this.click3 = this.click3.bind(this);
+    this.clickSubmit = this.clickSubmit.bind(this);
+    this.shuffle = this.shuffle.bind(this);
   }
 
   componentDidMount() {
     this.getRandomQuestion()
+  }
+
+  shuffle(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
   }
 
   getRandomQuestion() {
@@ -56,89 +64,71 @@ class App extends Component {
         console.log(err);
     }).then(results => {
       if (!results) return;
-      console.log(results)
+
+      var arrayRes = results["results"];
+
+      var arr = [];
+      while(arr.length < 3){
+          var r = Math.ceil(Math.random() * (arrayRes.length - 1));
+          if(arr.indexOf(r) === -1) arr.push(r);
+      }
+
+      console.log(arr);
+
+      var shuffledArray = [arrayRes[0], arrayRes[arr[0]], arrayRes[arr[1]], arrayRes[arr[2]]];
+      this.shuffle(shuffledArray);
+
+      console.log(shuffledArray);
+      
+      let correctIndex = shuffledArray.indexOf(arrayRes[0]);
+
+      this.setState({
+        question: questionRes,
+        query: queryRes,
+        choice0: shuffledArray[0],
+        choice1: shuffledArray[1],
+        choice2: shuffledArray[2],
+        choice3: shuffledArray[3],
+        correctIndex: correctIndex
+      })
+
       // Set the state of the genres list to the value returned by the HTTP response from the server.
     }, err => {
       // Print the error if there is one.
       console.log(err);
     });
-
-    //should send out query here
-    // update the index of the correct one
-    // randomly generate the other choices and locations
-    let correctIndex = Math.floor(Math.random() * 4);
-
-    this.setState({
-      question: questionRes,
-      query: queryRes,
-      choice0: "0",
-      choice1: "1",
-      choice2: "2",
-      choice3: "3",
-      correctIndex: correctIndex
-    })
   }
 
-  click0() {
-    console.log(this.state.correctIndex);
-    if (this.state.correctIndex == 0) {
-      this.setState({
-        result: "Correct!"
-      });
-      
-    } else {
-      this.setState({
-        result: "Incorrect"
-      });
+  clickSubmit() {
+
+    var anythingChecked = document.getElementById("a0").checked || (
+      document.getElementById("a1").checked) || (
+        document.getElementById("a2").checked) || (
+          document.getElementById("a3").checked);
+
+    if (anythingChecked) {
+      var correctChecked = document.getElementById("a" + this.state.correctIndex).checked;
+      if (correctChecked) {
+        this.setState({
+          result: "Correct!",
+          countCorrect: this.state.countCorrect + 1,
+          countQuestions: this.state.countQuestions + 1
+        });
+        
+      } else {
+        this.setState({
+          result: "Incorrect",
+          countQuestions: this.state.countQuestions + 1
+        });
+      }
+      this.getRandomQuestion();
+      document.getElementById("a0").checked = false;
+      document.getElementById("a1").checked = false;
+      document.getElementById("a2").checked = false;
+      document.getElementById("a3").checked = false;
     }
-    this.getRandomQuestion();
   }
 
-  click1() {
-    console.log(this.state.correctIndex);
-    if (this.state.correctIndex == 1) {
-      this.setState({
-        result: "Correct!"
-      });
-    } else {
-      this.setState({
-        result: "Incorrect"
-      });
-    }
-    this.getRandomQuestion();
-  }
-
-  click2() {
-    console.log(this.state.correctIndex);
-    if (this.state.correctIndex == 2) {
-      this.setState({
-        result: "Correct!"
-      });
-    } else {
-      this.setState({
-        result: "Incorrect"
-      });
-    }
-    this.getRandomQuestion();
-  }
-
-  click3() {
-    console.log(this.state.correctIndex);
-    if (this.state.correctIndex == 3) {
-      this.setState({
-        result: "Correct!"
-      });
-    } else {
-      this.setState({
-        result: "Incorrect"
-      });
-    }
-    this.getRandomQuestion();
-  }
-
-
-
-    // t
   callBackendAPI = async () => {
     const response = await fetch('/query');
     const body = await response.json();
@@ -153,31 +143,36 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <button onClick={this.getRandomQuestion}>
-            Start
-          </button>
-          <div>
-            {this.state.question}
-          </div>
-        
-          <button onClick={this.click0}>
-              Choice 0
-          </button>
-          <button onClick={this.click1}>
-              Choice 1
-          </button>
-          <button onClick={this.click2}>
-              Choice 2
-          </button>
-          <button onClick={this.click3}>
-              Choice 3
-          </button>
 
-          <div>
-            {this.state.result}
-          </div>
-        </header>
+        <h3>{this.state.question}</h3>
+      
+        <input type="radio" id="a0" name="answer" value="0"></input>
+        <label for="a0">
+          {this.state.choice0}
+        </label><br></br>
+        <input type="radio" id="a1" name="answer" value="1"></input>
+        <label for="a1">
+          {this.state.choice1}
+        </label><br></br>
+        <input type="radio" id="a2" name="answer" value="2"></input>
+        <label for="a2">
+          {this.state.choice2}
+        </label><br></br>
+        <input type="radio" id="a3" name="answer" value="3"></input>
+        <label for="a3">
+          {this.state.choice3}
+        </label><br></br><br></br>
+
+
+        <button onClick={this.clickSubmit}>
+          Submit
+        </button>
+
+        <h3>
+          {this.state.result}
+        </h3>
+
+        <h3>{this.state.countCorrect} for {this.state.countQuestions}! ({(100 * this.state.countCorrect / (this.state.countQuestions + 0.0000000001)).toFixed(1)}%)</h3>
 
         <p className="App-intro">{this.state.data}</p>
       </div>
