87c536bdb4c6889d7266275d56e9c6c84a550364
jackg235 <jackgoettle23@gmail.com>
2020-05-03 13:40:33 -0400

    retrieve stats

--- client/src/QuestionGenerator.js
+++ client/src/QuestionGenerator.js
@@ -1,28 +1,63 @@
-
 class QuestionGenerator {
 
 	stats = [];
 	years = [];
 	teams = [];
+	battingStats = [];
+	pitchingStats = [];
+	peopleAttributes = [];
+	college = [];
+	teams = [];
+	schools = [];
+	playoffBatting = []
 
 	constructor() {
+		//this.teams = this.getTeamOptions();
 		this.stats = this.getStatOptions();
 		this.years = this.getYearOptions();
-		this.teams = this.getTeamOptions();  
+		this.battingStats = this.getStats('BATTING', 5);
+		this.pitchingStats = this.getStats('PITCHING', 5);
+		this.peopleAttributes = this.getStats('PEOPLE', 0);
+		this.college = this.getStats('COLLEGEPLAYING', 0); 
+		this.teamStats = this.getStats('TEAMS', 5);
+		this.playoffBatting = this.getStats('PLAYOFFBATTING', 3);
+		this.schools = this.getStats('SCHOOLS', 0); 
 	}
-
 	// TODO get these from database call
 	getStatOptions() {
 		return ["home-runs", "wins"];
 	}
 
-	getBattingStats() {
-		return ['AB', 'R', 'Hits', '2B', '3B', 'HR', 'RBI', 'SB', 'CS', 'BB', 'SO']
-	}
-
-	getPitchingStats() {
-		return ['W', 'L', 'G', 'CG', 'SHO', 'IV', 'IPouts', 'H', 'ER', 'HR', 'BB', 'SO', 'ERA', 'IBB', 
-		'WP', 'HBP', 'BK', 'BFP', 'GF']
+	getStats(tableName, numSkip) {
+		console.log('getting stats for ' + tableName)
+		fetch('http://localhost:5000/query', {
+			method: 'post',
+			headers: {
+				'Content-Type': 'application/json',
+				'Accept': 'application/json'
+			},
+			body: JSON.stringify({
+				"query": `select column_name from all_tab_cols where TABLE_NAME = '${tableName}'`
+			})
+		}).then(res => {
+			return res.json();
+		}, err => {
+			console.log(err);
+		}).then(results => {
+			if (!results) return;
+			var stats2d = results["results"];
+			var stats = []
+			var i;
+			if (stats2d == null) {
+				console.log('error retrieving table ' + tableName)
+				return null
+			}
+			// ignore first few stats (player id, league id, etc.)
+			for (i = numSkip; i < stats2d.length; i++) {
+				stats.push(stats2d[i][0])
+			}
+			return stats
+		})
 	}
 
 	getYearOptions() {
@@ -30,12 +65,39 @@ class QuestionGenerator {
 	}
 
 	getTeamOptions() {
-		return ["Philadelphia Phillies", "New York Mets"];
+		console.log('getting team options...')
+		fetch('http://localhost:5000/query', {
+			method: 'post',
+			headers: {
+				'Content-Type': 'application/json',
+				'Accept': 'application/json'
+			},
+			body: JSON.stringify({
+				"query": 'select distinct name from teams'
+			})
+		}).then(res => {
+			return res.json();
+		}, err => {
+			console.log(err);
+		}).then(results => {
+			if (!results) return;
+			var stats2d = results["results"];
+			var stats = []
+			var i;
+			if (stats2d == null) {
+				console.log('error retrieving teams ')
+				return null
+			}
+			for (i = 0; i < stats2d.length; i++) {
+				stats.push(stats2d[i][0])
+			}
+			return stats
+		})
 	}
 
 	generateQuestion() {
 		var statIndex = Math.floor(Math.random() * this.stats.length);
-		var yearIndex = Math.floor(Math.random() *  this.years.length);
+		var yearIndex = Math.floor(Math.random() * this.years.length);
 		var teamIndex = Math.floor(Math.random() * this.teams.length);
 		var questionIndex = Math.floor(Math.random() * 7);
 		var stat = this.stats[statIndex];
@@ -45,84 +107,84 @@ class QuestionGenerator {
 		var question = "";
 		var query = "";
 
-		switch(questionIndex) {
-		  	case 0:
-		  		question = `Which player had the most ${stat} in ${year}?`;
-		  		query = `WITH Stat AS (` +
-							`SELECT name, ${stat}` +
-							` FROM Batting` +
-							` WHERE yearID == ${year}` +
-						`SELECT name, MAX(${stat})` +
-						` FROM Stat` +
-						` GROUP BY ${stat}` +
-						` HAVING ROWNUM == 1`;
-				return [question, query];
-		  	case 1:
-		    	question = `Which player had the fewest ${stat} in ${year}?`;
-		    	query = `WITH Stat AS (` +
-							`SELECT name, ${stat}` +
-							` FROM Batting` +
-							` WHERE yearID == ${year}` +
-						`SELECT name, MIN(${stat})` +
-						` FROM Stat` +
-						` GROUP BY ${stat}` +
-						` HAVING ROWNUM == 1`;
-
-		    	return [question, query];
-	   		case 2:
-		    	question = `Which pitcher had the most ${stat} in ${year}?`;
+		switch (questionIndex) {
+			case 0:
+				question = `Which player had the most ${stat} in ${year}?`;
 				query = `WITH Stat AS (` +
-							`SELECT name, ${stat}` +
-							` FROM Pitching` +
-							` WHERE yearID == ${year})` +
-						`SELECT name, MAX(${stat})` +
-						` FROM Stat` +
-						` GROUP BY ${stat}` +
-						` HAVING ROWNUM == 1`;
+					`SELECT name, ${stat}` +
+					` FROM Batting` +
+					` WHERE yearID == ${year}` +
+					`SELECT name, MAX(${stat})` +
+					` FROM Stat` +
+					` GROUP BY ${stat}` +
+					` HAVING ROWNUM == 1`;
+				return [question, query];
+			case 1:
+				question = `Which player had the fewest ${stat} in ${year}?`;
+				query = `WITH Stat AS (` +
+					`SELECT name, ${stat}` +
+					` FROM Batting` +
+					` WHERE yearID == ${year}` +
+					`SELECT name, MIN(${stat})` +
+					` FROM Stat` +
+					` GROUP BY ${stat}` +
+					` HAVING ROWNUM == 1`;
 
-		    	return [question, query];
-	   		case 3:
-	   			question = `Which pitcher had the fewest ${stat} in ${year}?`;
-	   			query = `WITH Stat AS (` +
-							`SELECT name, ${stat}` +
-							` FROM Pitching` +
-							` WHERE yearID == ${year}` +
-						`SELECT name, MIN(${stat})` +
-						` FROM Stat` +
-						` GROUP BY ${stat}` +
-						` HAVING ROWNUM == 1`;
+				return [question, query];
+			case 2:
+				question = `Which pitcher had the most ${stat} in ${year}?`;
+				query = `WITH Stat AS (` +
+					`SELECT name, ${stat}` +
+					` FROM Pitching` +
+					` WHERE yearID == ${year})` +
+					`SELECT name, MAX(${stat})` +
+					` FROM Stat` +
+					` GROUP BY ${stat}` +
+					` HAVING ROWNUM == 1`;
 
-	   			return [question, query];
-	   		case 4:
+				return [question, query];
+			case 3:
+				question = `Which pitcher had the fewest ${stat} in ${year}?`;
+				query = `WITH Stat AS (` +
+					`SELECT name, ${stat}` +
+					` FROM Pitching` +
+					` WHERE yearID == ${year}` +
+					`SELECT name, MIN(${stat})` +
+					` FROM Stat` +
+					` GROUP BY ${stat}` +
+					` HAVING ROWNUM == 1`;
+
+				return [question, query];
+			case 4:
 				question = `Which ${team} player holds the team record for the most ${stat} in a World Series?`;
 				query = `SELECT name, MAX(${stat})` +
-						` FROM world_series_batting` + 
-						` WHERE TEAM = ${team}` +
-						` GROUP BY name` +
-						` HAVING ROWNUM == 1`;
+					` FROM world_series_batting` +
+					` WHERE TEAM = ${team}` +
+					` GROUP BY name` +
+					` HAVING ROWNUM == 1`;
 
-	   			return [question, query];
-	   		case 5:
-	   			question = `Who is the lightest player to hit 50 home-runs in a season?`;
-	   			query = `WITH Weights AS (` +
-							`SELECT playerID, nameFirst, nameLast, weight` +
-							` FROM People)` +
-						`SELECT nameFirst, nameLast, MIN(weight)` +
-						` FROM Weights w JOIN Batting b ON b.playerID == w.playerID` +
-						` GROUP BY weight` +
-						` HAVING HR > 50`;
-	   			return [question, query];				 
+				return [question, query];
+			case 5:
+				question = `Who is the lightest player to hit 50 home-runs in a season?`;
+				query = `WITH Weights AS (` +
+					`SELECT playerID, nameFirst, nameLast, weight` +
+					` FROM People)` +
+					`SELECT nameFirst, nameLast, MIN(weight)` +
+					` FROM Weights w JOIN Batting b ON b.playerID == w.playerID` +
+					` GROUP BY weight` +
+					` HAVING HR > 50`;
+				return [question, query];
 			case 6:
 				question = `Which state has produced the most professional players?`;
 				query = `SELECT birthState, COUNT(birthState)` +
-						` FROM People` +
-						` GROUP BY birthState` +
-						` SORT BY COUNT(birthState) ASC` + 
-						` HAVING ROWNUM == 1`;
+					` FROM People` +
+					` GROUP BY birthState` +
+					` SORT BY COUNT(birthState) ASC` +
+					` HAVING ROWNUM == 1`;
 
 				return [question, query]
 
-		  	default:
+			default:
 
 		}
 	}
--- server/routes.js
+++ server/routes.js
@@ -1,5 +1,7 @@
 const oracledb = require('oracledb');
 const dbConfig = require('./db-config.js')
+const async = require('async')
+
 function connect() {
   return oracledb.getConnection({
       user: dbConfig.user,
@@ -12,8 +14,15 @@ function executeCmd(connection, cmd) {
   return connection.execute(cmd, []);
 }
 
-function connectAndQuery(query, callback) {
+
+/* -------------------------------------------------- */
+/* ------------------- Route Handlers --------------- */
+/* -------------------------------------------------- */
+
+function query(req, res) {
+  console.log('querying db...')
   let connection;
+  var query = req.body.query
   return connect().then(result => {
     connection = result;
     return executeCmd(connection, query);
@@ -21,74 +30,15 @@ function connectAndQuery(query, callback) {
     console.log(result.rows)
     return result.rows;
   }).then(result => {
-    return connection.close().then(() => callback(result));
-  });
-}
-
-/* -------------------------------------------------- */
-/* ------------------- Route Handlers --------------- */
-/* -------------------------------------------------- */
-
-const queryPromise = (fileName) => {
-  return new Promise((resolve, reject) => {
-    if(typeof fileName !== 'string') {
-      return reject('Argument should be string')
-    }
-
-    fs.readFile(fileName, 'utf8', (err, data) => {
-      if(err) {
-        return reject(err)
-      }
-      const lines = data.trim().split('\n');
-      resolve(lines)
-    })
+    return connection.close().then(() => res.json({'results' : result}));
+  }).catch(error => {
+    console.log('ERROR executing query: ' + query)
+    console.log(error)
   })
 }
 
 
 
-
-function query(req, res) {
-  var q = req.body.query
-  var dummy = 'select nameGiven from People where rownum < 11';
-  connectAndQuery(dummy, function(result) {
-    res.json({'results' : result})
-  });
-};
-
-// currently doesn't work
-function headerCallback(callback) {
-  var headerArr = []
-  var tables = ['BATTING', 'PITCHING', 'PEOPLE', 'COLLEGEPLAYING', 'TEAMS', 'SCHOOL', 'PLAYOFFBATTING']
-  var i;
-  for (i = 0; i < tables.length; i++) {
-    console.log('retrieving ' + tables[i])
-    var query = `select column_name from all_tab_cols where TABLE_NAME = '${tables[i]}'`
-    connectAndQuery(query, function(result) {
-      headerArr.push({
-        key : tables[i],
-        value : result
-      })
-      console.log(headerArr)
-    });
-  }
-  callback(headerArr)
-}
-
-
-function get_headers(req, res) {
-  res.json({'in progress' : 'in progress'})
-  return
-  headerCallback(function(result) {
-    console.log('headers')
-    console.log(result)
-    console.log('headers done')
-    res.json({'results' : result})
-  });
-}
-
-
 module.exports = {
   query : query,
-  get_headers : get_headers
 }
